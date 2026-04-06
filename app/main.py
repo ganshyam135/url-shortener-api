15c94992a941c6948d769765fd3bbe9443c03f2c
Ganshyam Suthar <ganshyamsuthar03@gmail.com>
2026-04-06 19:51:42 +0530

    feat: setup database connection and URL model

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from app.db.database import Base, engine
+from app.models import url
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
---
+++ app/models/url.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
+from app.db.database import Base
+
+
+class URL(Base):
+    __tablename__ = 'urls'
+
+    id = Column(Integer, primary_key=True, index=True)
+    original_url = Column(String, nullable=False)
+    short_code = Column(String, unique=True, index=True)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    clicks = Column(Integer, default=0)
